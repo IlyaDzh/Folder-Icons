a6d760964ac61d64c98a14b6f0bfc6edfca40d1d
Илья Долженко <ilay00@mail.ru>
2019-07-18 16:53:06 +0300

    Add restart explorer

--- SwapFolders/Form1.Designer.cs
+++ SwapFolders/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonOpenFIle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxIcon = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 47);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(134, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonOpenFIle
+            // 
+            this.buttonOpenFIle.Location = new System.Drawing.Point(332, 13);
+            this.buttonOpenFIle.Name = "buttonOpenFIle";
+            this.buttonOpenFIle.Size = new System.Drawing.Size(40, 47);
+            this.buttonOpenFIle.TabIndex = 2;
+            this.buttonOpenFIle.Text = "File";
+            this.buttonOpenFIle.UseVisualStyleBackColor = true;
+            this.buttonOpenFIle.Click += new System.EventHandler(this.buttonOpenFIle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // listBoxIcon
+            // 
+            this.listBoxIcon.FormattingEnabled = true;
+            this.listBoxIcon.Location = new System.Drawing.Point(12, 117);
+            this.listBoxIcon.Name = "listBoxIcon";
+            this.listBoxIcon.Size = new System.Drawing.Size(360, 134);
+            this.listBoxIcon.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(384, 415);
+            this.Controls.Add(this.listBoxIcon);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOpenFIle);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonOpenFIle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxIcon;
     }
 }
 
--- SwapFolders/Form1.cs
+++ SwapFolders/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,53 @@ namespace SwapFolders
 {
     public partial class Form1 : Form
     {
+        string filePath = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void RestartExplorer()
+        {
+            Process[] explorer = Process.GetProcessesByName("explorer");
+            foreach (Process process in explorer)
+            {
+                process.Kill();
+            }
+            Process.Start("explorer.exe");
+        }
+
+        void SwapIcon()
+        {
+            DirectoryInfo folder = new DirectoryInfo($@"{textBox1.Text}\");
+            filePath = folder.FullName + "desktop.ini";
+            File.Delete(filePath);
+
+            using (StreamWriter file = new StreamWriter(filePath))
+            {
+                file.WriteLine("[.ShellClassInfo]");
+                file.WriteLine(@"IconFile=D:\Swap Folders\icl\fColors.icl");
+                file.WriteLine("IconIndex = 3");
+            }
+            folder.Attributes = folder.Attributes | FileAttributes.ReadOnly;
+            File.SetAttributes(filePath, FileAttributes.Hidden | FileAttributes.System | FileAttributes.Archive);
+
+            RestartExplorer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SwapIcon();
+        }
+
+        private void buttonOpenFIle_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
         }
     }
 }
